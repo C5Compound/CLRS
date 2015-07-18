eb43cf4226459f36205b64f9220a762443e03d29
Karel <C5Compound@hotmail.com>
2015-07-18 14:45:54 +0800

    树的比较测试可用

--- TREE/tree.h
+++ TREE/tree.h
@@ -10,8 +10,13 @@ struct node
     node *right;
 };
 
+// 用数组初始化一棵树
 node* tree_build(int ls[], int length, int i = 1);
+// 以图形的方式输出一棵树
 void tree_print(Agraph_t *graph, Agnode_t *pre, node *root);
+// 比较两棵树是不是相同
 bool tree_compare(node *ra, node *rb);
+// 释放树所占内存
 void tree_free(node *root);
+// 对树作镜像
 node* tree_mirror(node *root);
--- TestOnly/TestOnly/source.cpp
+++ TestOnly/TestOnly/source.cpp
@@ -10,16 +10,19 @@ int main()
     graph = agopen("graph", Agdirected, NULL);
 
     int ls[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
-    node *root = tree_build(ls, 31);
-    node *newtree = tree_mirror(root);
-    tree_print(graph, NULL, newtree);
-    tree_free(root);
-    tree_free(newtree);
+    int ls2[] = { 0, 1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+    node *tree1 = tree_build(ls, 31);
+    node *tree2 = tree_build(ls2, 31);
+    if (tree_compare(tree1, tree2)) {
+        tree_print(graph, NULL, tree2);
+    }
+    tree_free(tree1);
+    tree_free(tree2);
 
     /* Compute a layout using layout engine from command line args */
     gvLayout(gvc, graph, "dot");
     /* Write the graph according to -T and -o options */
-    gvRenderFilename(gvc, graph, "png", "demo1.png");
+    gvRenderFilename(gvc, graph, "png", "demo2.png");
     /* Free layout data */
     gvFreeLayout(gvc, graph);
     /* Free graph structures */
