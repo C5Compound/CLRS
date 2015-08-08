0197da4597b33c0b6afa4902c1faf609e1d49f9c
Karel <C5Compound@hotmail.com>
2015-08-08 17:52:09 +0800

    heaplify ok

--- SORT/external-sort.cpp
+++ SORT/external-sort.cpp
@@ -4,17 +4,48 @@
  *  2.置换选择排序
  */
 
- // 堆调整函数
-void heaplify()
+void swap(int &p, int &q) 
 {
+    int k = p;
+    p = q;
+    q = k;
+}
 
+ // 最小堆调整函数
+void heaplify(int a[], int i, int size)
+{
+    int l = 2 * i; r = 2 * i + 1, m;
+    m = (l <= size && a[l] < a[i]) ? l : i;
+    m = (r <= size && a[r] < a[m]) ? r : m;
+    if (m != i) {
+        swap(a[m], a[i]);
+        heaplify(a, m, size);
+    }
 }
 
 // 堆建立函数
-void heapbuild()
+void heapbuild(int a[], int size)
 {
-
+    for (int i = size >> 1; i >= 1; ++i) {
+        heaplify(a, i, size);
+    }
 }
+
+// 败者树调整函数
+
+
+
+/**
+ *  败者树建立函数，败者树为完全二叉树
+ *  建立过程与堆建立过程相似
+ */
+void loserTree(int a[], int k)
+{
+    for (int i = k; i >= 0; ++i) {
+        ajustLosers(a, i, 2 * k);
+    }
+}
+
 
 // K路归并
 int k_merge()
@@ -27,14 +58,13 @@ int k_merge()
 // 快排划分 升序
 int partition(int a[], int l, int r)
 {
-    int i = l, j = l;
-    static auto swap = [&](int p, int q){int k = p; p = q; q = k;};
+    int i = l, j = l + 1;
     for (; j <= r; ++j) {
-        if (a[j] < a[i]) {
-            swap(a[i], a[j]);
-            ++i;
+        if (a[j] < a[l]) {
+            swap(a[++i], a[j]);
         }
     }
+    swap(a[i], a[l]);
     return i;
 }
 
