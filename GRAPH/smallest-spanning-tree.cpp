fe30ef15a043f6ee4d99cb2ead5b98f8566d2e62
Karel <C5Compound@hotmail.com>
2015-08-13 22:22:36 +0800

    kruskal

--- GRAPH/smallest-spanning-tree.cpp
+++ GRAPH/smallest-spanning-tree.cpp
@@ -1,19 +1,27 @@
+struct Edge
+{
+    char x, y;
+    int weight;
+    Edge(char a, char b, int c) : x(a), y(b), weight(c) {};
+};
+
 /**
- *  最小生成树
- *  Kruskal算法的实现依赖于并查集
- *  Prim算法
- *  邻接表表示和邻接矩阵表示
- */
+*  最小生成树
+*  Kruskal算法的实现依赖于并查集
+*  Prim算法
+*  邻接表表示和邻接矩阵表示
+*/
 #define SET_MAX 2048
 int parent[SET_MAX];
-int rank[SET_MAX];
+int Rank[SET_MAX];
 
 void make_set(int x)
 {
     parent[x] = x;
-    rank[x] = 0;
+    Rank[x] = 0;
 }
 
+// 路径压缩
 int find_set(int x)
 {
     if (parent[x] != x) {
@@ -22,18 +30,41 @@ int find_set(int x)
     return parent[x];
 }
 
+// 按秩合并
 void union_set(int x, int y)
 {
-    parent[find_set(x)] = find_set(y);
+    int fx = find_set(x), fy = find_set(y);
+    if (Rank[fx] > Rank[fy]) {
+        parent[fy] = fx;
+    }
+    else {
+        parent[fx] = fy;
+    }
+    if (Rank[fx] == Rank[fy]) {
+        Rank[fy] += 1;
+    }
 }
 
-void Kruskal(vector<vector<int>> &graph)
+/**
+* G(V,E), V为节点，char型,
+* 图以(u v weight)vector的形式输入
+*/
+void Kruskal(vector<char> &vertexs, vector<Edge> &v2emap)
 {
-    
+    for (int i = 0; i < vertexs.size(); ++i) {
+        make_set(vertexs[i]);
+    }
+    // 升序排列
+    sort(v2emap.begin(), v2emap.end(), [](Edge x, Edge y){return x.weight < y.weight; });
+    for (auto i : v2emap) {
+        if (find_set(i.x) != find_set(i.y)) {
+            printf("edge from %c to %c with weight %d\n", i.x, i.y, i.weight);
+            union_set(i.x, i.y);
+        }
+    }
 }
-
 
 void Prim(vector<vector<int>> &graph)
 {
-
+    
 }
