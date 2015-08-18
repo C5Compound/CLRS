cadb8bf28aee604b7012695a14071f974511bfd3
C5Compound <C5Compound@hotmail.com>
2015-08-18 11:42:01 +0800

    dijkstra

--- GRAPH/single source shortest path.cpp
+++ GRAPH/single source shortest path.cpp
@@ -6,7 +6,22 @@ struct node
 {
     int v, w;
     node(int iv, int iw) : v(iv), w(iw) {};
+    bool operator < (const node &other) {
+        return other.w < w;
+    }
 };
+
+// 格式化输出结果
+void formate(vector<char> &vertexs, vector<int> &dis, vector<int> pa, int s)
+{
+    for (int i = 0; i < vertexs.size(); ++i) {
+        printf("dis of %c is: %d\n", vertexs[i], dis[i]);
+        for (int j = i; j != s; j = pa[j]) {
+            printf("%c<--", vertexs[j]);
+        }
+        printf("%c\n", vertexs[s]);
+    }
+}
 
 // Bellman-Ford算法
 bool BellmanFord(vector<char> &vertexs, vector<vector<node>> &adjs, int s)
@@ -34,14 +49,7 @@ bool BellmanFord(vector<char> &vertexs, vector<vector<node>> &adjs, int s)
             }
         }
     }
-    // 格式化输出结果
-    for (int i = 0; i < adjs.size(); ++i) {
-        printf("dis of %c is: %d\n", vertexs[i], dis[i]);
-        for (int j = i; j != s; j = pa[j]) {
-            printf("%c<--", vertexs[j]);
-        }
-        printf("%c\n", vertexs[s]);
-    }
+    formate(vertexs, dis, pa, s);
     return true;
 }
 
@@ -80,7 +88,7 @@ void DFS(vector<vector<node>> &adjs, vector<bool> &visited, vector<int> &topolog
         return;
     }
     for (auto i : adjs[u]) {
-        DFS(adjs, visited, topology, i);
+        DFS(adjs, visited, topology, i.v);
     }
     visited[u] = true;
     topology.push_back(u);
@@ -95,23 +103,76 @@ void TopologySort(vector<vector<node>> &adjs, vector<int> &topology)
         }
     }
 }
-
-void DagShortestPaths(vector<char> &vertexs, vector<vector<node>> &adjs)
+// 求最短路径
+void DagShortestPaths(vector<char> &vertexs, vector<vector<node>> &adjs, int s)
 {
-    vector<int> dis(adjs.size(), INT_MAX), pa(adjs.size(), 0), topology;
-    DFS(adjs, topology);
+    vector<int> dis(adjs.size(), INT_MAX), pa(adjs.size(), s), topology;
+    TopologySort(adjs, topology);
     dis[0] = 0;
     for (int i = topology.size() - 1; i >= 0; --i) {
-        for (auto j : adjs[i]) {
+        for (auto j : adjs[topology[i]]) {
             // 松弛过程，带无穷判断
-            if (dis[i] < INT_MAX && dis[j.v] > dis[i] + j.w) {
-                dis[j.v] = dis[i] + j.w;
-                pa[j.v] = i;
+            if (dis[topology[i]] < INT_MAX && dis[j.v] > dis[topology[i]] + j.w) {
+                dis[j.v] = dis[topology[i]] + j.w;
+                pa[j.v] = topology[i];
             }
         }
     }
-    // 格式化输出结果
-    
+    formate(vertexs, dis, pa, s);
 }
 
-// Dijkstra算法
+void testDagShortestPaths()
+{
+    char v[6] = {'s', 'r', 't', 'x', 'y', 'z'};
+    vector<char> vertexs(v, v + 6);
+    vector<vector<node>> adjs;
+    vector<node> tmp;
+    tmp.push_back(node(2, 2));
+    tmp.push_back(node(3, 6));
+    adjs.push_back(tmp);
+    tmp.clear();
+    tmp.push_back(node(0, 5));
+    tmp.push_back(node(2, 3));
+    adjs.push_back(tmp);
+    tmp.clear();
+    tmp.push_back(node(3, 7));
+    tmp.push_back(node(4, 4));
+    tmp.push_back(node(5, 2));
+    adjs.push_back(tmp);
+    tmp.clear();
+    tmp.push_back(node(4, -1));
+    tmp.push_back(node(5, 1));
+    adjs.push_back(tmp);
+    tmp.clear();
+    tmp.push_back(node(5, -2));
+    adjs.push_back(tmp);
+    tmp.clear();
+    adjs.push_back(tmp);
+    DagShortestPaths(vertexs, adjs, 0);
+}
+
+// Dijkstra算法，基于最小优先级队列
+void dijkstra(vector<char> &vertexs, vector<vector<node>> &adjs, int s)
+{
+    vector<int> dis(adjs.size(), INT_MAX), pa(adjs.size());
+    vector<bool> visited(adjs.size(), false);
+    priority_queue<node> Q;
+    // 初始化
+    Q.push(s, 0);
+    while (!Q.empty()) {
+        node it = Q.top();
+        Q.pop();
+        if (visited[it.v]) {
+            continue;
+        }
+        visited[it.v] = true;
+        for (auto i : adjs[it.v]) {
+            if (!visited[i.v] && dis[i.v] > dis[it.v] + i.w) {
+                dis[i.v] = dis[it.v] + i.w;
+                pa[i.v] = it.v;
+                Q.push(node(i.v, dis[i.v]));
+            }
+        }
+    }
+    formate(vertexs, dis, pa, s);
+}
