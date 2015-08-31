44d473788be0b0df08db0e0f1d26ba739ee3ad6d
Karel <C5Compound@hotmail.com>
2015-09-01 00:21:35 +0800

    9/1/00/21

--- SORT/shell-sort.cpp
+++ SORT/shell-sort.cpp
@@ -7,7 +7,11 @@ void shellsort(int arr[], int len)
 {
     for (int step = len >> 1; step > 1; step >>= 1) {
         for (int i = step;  i < len; ++i) {
-
+            int tmp = arr[i];
+            for (int j = i - step; j >= 0 && arr[j] > tmp; j -= step) {
+                arr[j + step] = arr[j];
+            }
+            arr[j + step] = tmp;
         }
     }
 }
