893c5b6af6acbb5e458a2a1651d347e064532f8a
C5Compound <C5Compound@hotmail.com>
2015-09-05 11:18:25 +0800

    RMQ测试OK

--- LCA&RMQ/RMQ.cpp
+++ LCA&RMQ/RMQ.cpp
@@ -21,17 +21,43 @@ void DP4RMQ(int A[], vector<vector<int>> &dp)
  *  Sparse Table 稀疏表方法，预处理时间为O(NlogN)，查询时间为O(1)，空间复杂度为O(NlogN)
  *  表中保存的是最小值对应的索引，可改为保存最小值
  */
+// 表中保存最小值的索引
 void SparseTable(int A[], vector<vector<int>> &dp)
 {
     for (int i = 0; dp.size() >= 1 << i; ++i) {
         for (int j = 0; j + (1 << i) - 1 < dp.size(); ++j) {
-            dp[j][i] = A[dp[j][i - 1]] < A[dp[j + (1 << (i - 1))][i - 1]] ? dp[j][i - 1] : dp[j + (1 << (i - 1))][i - 1];
+            if (i > 0) {
+                dp[j][i] = A[dp[j][i - 1]] < A[dp[j + (1 << i - 1)][i - 1]] ? dp[j][i - 1] : dp[j + (1 << (i - 1))][i - 1];
+            }
+            else {
+                dp[j][i] = j;
+            }
         }
     }
 }
-
+// 稀疏表查询过程
 int querySparseTable(int A[], vector<vector<int>> &dp, int i, int j)
 {
     int k = (int)floor(log2(j - i + 1));
-    return min(A[dp[i][k]], A[dp[j - (2 << k) + 1][k]]);
+    return A[dp[i][k]] < A[dp[j - (1 << k) + 1][k]] ? dp[i][k] : dp[j - (1 << k) + 1][k];
+}
+
+// 表中保存最小值
+void SparseTable(int A[], vector<vector<int>> &dp)
+{
+    for (int i = 0; 1 << i <= dp.size(); ++i) {
+        for (int j = 0; j + (1 << i) - 1 < dp.size(); ++j) {
+            if (i > 0) {
+                dp[j][i] = min(dp[j][i - 1], dp[j + (1 << i - 1)][i - 1]);
+            } else {
+                dp[j][i] = A[j];
+            }
+        }
+    }
+}
+// 查询
+int querySparseTable(int A[], vector<vector<int>> &dp, int i, int j)
+{
+    int k = (int)floor(log2(j - i + 1));
+    return min(dp[i][k], dp[j - (1 << k) + 1][k]);
 }
